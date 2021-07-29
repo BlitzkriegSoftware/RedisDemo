6b09eee4c23df0318884ce6c402f6fa704bd66bb
Stuart Williams <spookdejur@hotmail.com>
2021-07-29 15:47:05 -0700

    WIP

--- Blitz.Redis.Library/BlitzRedisClient.cs
+++ Blitz.Redis.Library/BlitzRedisClient.cs
@@ -12,9 +12,20 @@ namespace Blitz.Redis.Library
     /// </summary>
     public class BlitzRedisClient
     {
+
+        /// <summary>
+        /// Default: REDIS DB (-1)
+        /// </summary>
+        public const int RedisDefaultDb = -1;
+
+        /// <summary>
+        /// Default: REDIS Server (0)
+        /// </summary>
+        public const int RedisDefaultServer = 0;
+
         private readonly Models.RedisConfiguration _config;
 
-        private readonly ConnectionMultiplexer redis;
+        private readonly ConnectionMultiplexer _redisConnectMultiplexer;
 
         [ExcludeFromCodeCoverage]
         private BlitzRedisClient() { }
@@ -25,8 +36,8 @@ namespace Blitz.Redis.Library
         /// <param name="config">RedisConfiguration</param>
         public BlitzRedisClient(Models.RedisConfiguration config)
         {
-            _config = config ?? throw new ArgumentNullException(nameof(config));
-            redis = ConnectionMultiplexer.Connect($"{_config.ConnectionString}");
+            this._config = config ?? throw new ArgumentNullException(nameof(config));
+            this._redisConnectMultiplexer = ConnectionMultiplexer.Connect($"{this._config.ConnectionString}");
         }
 
         /// <summary>
@@ -34,9 +45,10 @@ namespace Blitz.Redis.Library
         /// </summary>
         /// <param name="key">key</param>
         /// <param name="value">value</param>
-        public void Set(string key, string value)
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
+        public void Set(string key, string value, int dbIndex = RedisDefaultDb)
         {
-            IDatabase db = redis.GetDatabase();
+            IDatabase db = this._redisConnectMultiplexer.GetDatabase(dbIndex);
             db.StringSet(key, value);
         }
 
@@ -46,9 +58,10 @@ namespace Blitz.Redis.Library
         /// <typeparam name="T">Type</typeparam>
         /// <param name="key">key</param>
         /// <param name="thing">of T</param>
-        public void Set<T>(string key, T thing) where T : new()
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
+        public void Set<T>(string key, T thing, int dbIndex = RedisDefaultDb) where T : new()
         {
-            IDatabase db = redis.GetDatabase();
+            IDatabase db = this._redisConnectMultiplexer.GetDatabase(dbIndex);
             var json = JsonConvert.SerializeObject(thing);
             db.StringSet(key, json);
         }
@@ -57,10 +70,11 @@ namespace Blitz.Redis.Library
         /// Get
         /// </summary>
         /// <param name="key">key</param>
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
         /// <returns>value</returns>
-        public string Get(string key)
+        public string Get(string key, int dbIndex = RedisDefaultDb)
         {
-            IDatabase db = redis.GetDatabase();
+            IDatabase db = this._redisConnectMultiplexer.GetDatabase(dbIndex);
             return db.StringGet(key);
         }
 
@@ -69,13 +83,81 @@ namespace Blitz.Redis.Library
         /// </summary>
         /// <typeparam name="T">Type</typeparam>
         /// <param name="key">key</param>
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
         /// <returns>instance of T</returns>
-        public T Get<T>(string key)
+        public T Get<T>(string key, int dbIndex = RedisDefaultDb)
         {
-            IDatabase db = redis.GetDatabase();
+            IDatabase db = this._redisConnectMultiplexer.GetDatabase(dbIndex);
             var json = db.StringGet(key);
             T value = JsonConvert.DeserializeObject<T>(json);
             return value;
+        }
+
+        /// <summary>
+        /// Delete a key/value, ignores not found
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
+        public void Delete(string key, int dbIndex = RedisDefaultDb)
+        {
+            IDatabase db = this._redisConnectMultiplexer.GetDatabase(dbIndex);
+            db.KeyDelete(key);
+        }
+
+        /// <summary>
+        /// Clear All Keys
+        /// <para>Warning! Use carefully</para>
+        /// </summary>
+        /// <param name="serverIndex">(optional) Server Index, default <c>RedisDefaultServer</c></param>
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
+        public void ClearAll(int serverIndex = RedisDefaultServer, int dbIndex = RedisDefaultDb)
+        {
+            var endpoint = this._redisConnectMultiplexer.GetEndPoints()[serverIndex];
+            var redisServer = this._redisConnectMultiplexer.GetServer(endpoint);
+            redisServer.FlushDatabase(dbIndex);
+        }
+
+
+        /// <summary>
+        /// Delete keys based on a key regex expression (case sensitive) or you can pass a literal string
+        /// <para>
+        /// <list type="bullet">
+        /// <listheader>
+        /// <term>Expression</term>
+        /// <description>Matches</description>
+        /// </listheader>
+        /// <item>
+        /// <term><![CDATA[?]]></term>
+        /// <description>Matches single character</description>
+        /// </item>
+        /// <item>
+        /// <term><![CDATA[*]]></term>
+        /// <description>Matches any number of characters (good for begins with or ends with)</description>
+        /// </item>
+        /// <item>
+        /// <term><![CDATA[[{chars}]]]></term>
+        /// <description>Matches chars in list</description>
+        /// </item>
+        /// <item>
+        /// <term><![CDATA[\]]></term>
+        /// <description>Escape litteral chars</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        /// <param name="keyMatchExpression">Simple RegEx</param>
+        /// <param name="dbIndex">(optional) index, default <c>RedisDefaultDb</c></param>
+        /// <returns>Count of deleted keys</returns>
+        public long ClearMatching(string keyMatchExpression, int dbIndex = RedisDefaultDb)
+        {
+            long deleted = 0;
+            IDatabase db = this._redisConnectMultiplexer.GetDatabase(dbIndex);
+            var matches = (RedisKey[])db.Execute("KEYS", keyMatchExpression);
+            if ((matches != null) && (matches.Length > 0))
+            {
+                deleted = db.KeyDelete(matches);
+            }
+            return deleted;
         }
 
         /// <summary>
@@ -85,7 +167,7 @@ namespace Blitz.Redis.Library
         {
             get
             {
-                return _config;
+                return this._config;
             }
         }
 
